2135c7447a9c84fd3a74ad27b77f9e2b6de304da
Mameesan <65023337+Mameesan@users.noreply.github.com>
2020-05-08 21:57:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,35 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author.id == 159985870458322944: # MEE6からのメッセージかどうかを判別
+        if message.content.startswith("!levelup"):
+            await message.delete() # メッセージを消去
+
+            level = int(message.content.split()[-2]) # メッセージを分解
+            t_name = message.content.split()[-1] # メッセージを分解
+            target = discord.utils.get(message.server.members, mention=t_name) # レベルアップしたユーザーのIDを取得
+
+            replys = f"{t_name}さん、が{str(level)}レべになりました。" # レベルアップメッセージ
+            await message.channel.send(replys)
+
+            if level == 1: # レベル1になった時の処理
+                levelrole1 = discord.utils.get(message.server.roles, name="初心者")
+                await target.add_roles(levelrole1)
+
+            elif level == '5': # レベル5になった時の処理
+                levelrole1 = discord.utils.get(message.server.roles, name="初心者")
+                levelrole5 = discord.utils.get(message.server.roles, name="アマチュア")
+                await target.add_roles(levelrole5)
+                await target.remove_roles(levelrole1)
+
+client.run("") # NzA4MjI5MTk5MTM5MzA3NTYw.XrVPwQ.CDG6QKO_iRcrSONuBHWb4yBnn5M
 
 
-bot.run(token)
+bot.run(NzA4MjI5MTk5MTM5MzA3NTYw.XrVPwQ.CDG6QKO_iRcrSONuBHWb4yBnn5M)
